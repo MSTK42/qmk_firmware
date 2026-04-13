1d27b872dd7fd4cb03c74b7e79c02e6d6f490986
JEEBIS27 <jeebis.iox@gmail.com>
2026-04-13 12:41:19 +0900

    メジロ式にFirst Up Chord Send機能を追加。変数で切り替え可能。

--- keyboards/jeebis/mejiro31/mejiro_fifo.h
+++ keyboards/jeebis/mejiro31/mejiro_fifo.h
@@ -6,6 +6,10 @@
 #include QMK_KEYBOARD_H
 
 // Public API
+// true: 何かキーが離された時点で確定（First Up Chord Send）
+// false: すべてのキーが離された時点で確定
+extern bool mejiro_first_up_chord_send;
+
 bool is_stn_key(uint16_t kc);
 void mejiro_on_press(uint16_t kc);
 void mejiro_on_release(uint16_t kc);
